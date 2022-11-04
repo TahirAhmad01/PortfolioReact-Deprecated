662aa880f442dc2939639740fbd9abd7bc7d5206
TahirAhmad01 <tahirahmad0234@gmail.com>
2022-11-05 00:02:11 +0600

    flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+    require("flowbite/plugin"),
+    require("tailwindcss"),
+    require("autoprefixer"),
   ],
 };
